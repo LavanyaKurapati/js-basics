03a43c06df46703b1a286cad17758a9c6904b905
Lavanya <lavanyakurapati315@gmail.com>
2021-11-17 14:56:33 +0530

    challenge1

---
+++ index.js
@@ -0,0 +1,80 @@
+
+//-------- Problem1 -------------
+
+// TODO destructure the array so that 2 variables apple and peach hold values at indexes 0 and 2 in the array
+const numbersArray = ['apple','orange', 'peach'];
+
+const [apple, orange, peach] = numbersArray;
+console.log(apple)
+console.log(orange)
+console.log(peach)
+
+
+// TODO destructure the object to extract name and age of the person 
+const person = { 
+  name: 'abcde', 
+  age: 10, 
+  gender: 'm' 
+}; 
+
+const { name, age } = person;
+console.log(name)
+console.log(age)
+
+//--------- Problem2 --------
+
+// TODO combine below arrays into one array array3 
+const array1 = [1,2,3]; 
+const array2 = [4,5,6]; 
+// array3 should be [1,2,3,4,5,6]
+const array3 = [...array1, ...array2];
+console.log(array3)
+ 
+ 
+ // TODO combine below objects into one personDetails object 
+const basicDetails = { 
+  name: 'abcde', 
+  age: 10, 
+  gender: 'm' 
+}; 
+ 
+const educationDetails = { 
+  degree:'xyz', 
+  college:'anc', 
+}
+
+const personDetails = { ...basicDetails, ...educationDetails}
+console.log(personDetails)
+
+
+//--------- Problem3 --------
+
+function largestNumber (numArray){ 
+  // TODO complete this 
+  let largeNum=0
+  for (let i=0; i<numArray.length-1; i++){
+    largeNum = numArray[i]>numArray[i+1] ? numArray[i] : numArray[i+1]
+  }
+  return largeNum
+} 
+ 
+let numArray1 = [1,5,7,9]
+let result1 = largestNumber(numArray1)
+console.log(result1)
+let numArray2 = [-1,-5,0,-100] 
+let result2 = largestNumber(numArray2)
+console.log(result2)
+
+
+//------------ Problem4 -----------
+
+function updateSalaries (employeesArray, percent){ 
+  // TODO complete this 
+  let resultArray = []
+  resultArray = employeesArray.map((item) => item + (item/percent))
+  return resultArray
+} 
+  
+let employeesArray = [100,500,700,400], percent = 10 
+result = updateSalaries(employeesArray, percent)
+console.log(result)
